f2db37421135f9db550d879d431d9aec1d46baac
OTRAY271 <mojayarto@gmail.com>
2023-10-22 22:25:01 +0900

    feat: 各呼び出しの実装

--- app/infer_engine.py
+++ app/infer_engine.py
@@ -4,18 +4,22 @@ import numpy as np
 import torch
 
 from spinvae.data import build
+from spinvae.data.preset2d import Preset2d
 from spinvae.evaluation.load import ModelLoader
 from spinvae.model import hierarchicalvae
 
 
 class InferEngine:
+    SEARCH_LENGTH = 113 * 0.075
+
     def __init__(
         self,
         model_path=Path("pretrained_weight/decoder_checkpoint.pth"),
         remain_size=50,
-        remain_rows=16,
+        remain_rows=32,
     ) -> None:
         self.ae_model = self.__load_model(model_path)
+        self.dataset = build.get_dataset(*ModelLoader.get_model_train_configs(None))
         self.remain_idx = np.random.choice(257 * 251, remain_size, replace=False)
         self.remain_rows = remain_rows
 
@@ -45,3 +49,28 @@ class InferEngine:
         jacobian = torch.func.jacrev(self.__decode)(z_tensor).detach().numpy()
         _, sigma, vh = np.linalg.svd(jacobian, full_matrices=True)
         return vh[: self.remain_rows].tolist(), sigma[: self.remain_rows].tolist()
+
+    def calc_prob(self, sigma: list[float]) -> list[float]:
+        sigma_np = np.array(sigma)
+        sum_sigma = np.sum(sigma_np)
+        if sum_sigma != 0:
+            return sigma_np / sum_sigma
+        else:
+            return np.ones_like(sigma_np) / len(sigma_np)
+
+    def infer_synth_params(self, z: torch.Tensor) -> list[float]:
+        zs = z.unsqueeze(0)
+        zs = self.ae_model.unflatten_latent_values(zs)
+        u_out = self.ae_model.decoder.preset_decoder(zs)[0][0]
+        preset2d = Preset2d(self.dataset, learnable_tensor_preset=u_out)
+        params = list(preset2d.to_raw())
+        return params
+
+    def turn_around(self, vh: list[list[float]], prob: list[float]) -> list[float]:
+        return vh[np.random.choice(len(prob), size=1, p=prob)[0]]
+
+    def calc_z(
+        self, position: float, basis: list[float], center_z: list[float]
+    ) -> torch.Tensor:
+        basis_tensor, center_z_tensor = torch.tensor(basis), torch.tensor(center_z)
+        return center_z_tensor + position * InferEngine.SEARCH_LENGTH * basis_tensor
--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
-from mangum import Mangum
+import json
+
+from fastapi import Depends, FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
 from app.infer_engine import InferEngine
@@ -8,15 +11,59 @@ infer_engine = InferEngine()
 app = FastAPI()
 
 
-class VhAndSigma(BaseModel):
+class CommonRequest(BaseModel):
+    position: float
+    basis: list[float]
+    center_z: list[float]
+
+
+class SeekResponse(BaseModel):
+    synth_params: list[float]
+
+
+class MoveForwardResponse(BaseModel):
     vh: list[list[float]]
-    sigma: list[float]
+    prob: list[float]
+    new_center_z: list[float]
 
 
-@app.put("/calc_vh_and_sigma")
-def calc_vh_and_sigma(z: list[float]) -> VhAndSigma:
+class TurnAroundRequest(BaseModel):
+    vh: list[list[float]]
+    prob: list[float]
+
+
+class TurnAroundResponse(BaseModel):
+    basis: list[float]
+
+
+async def parse_body(request: Request) -> dict:
+    data: bytes = await request.body()
+    return json.loads(data.decode("utf-8"))
+
+
+@app.post("/move_foward")
+def move_forward(data: dict = Depends(parse_body)) -> MoveForwardResponse:
+    req = CommonRequest(**data)
+    z = infer_engine.calc_z(req.position, req.basis, req.center_z)
     vh, sigma = infer_engine.calc_vh_and_sigma(z)
-    return VhAndSigma(vh=vh, sigma=sigma)
+    prob = infer_engine.calc_prob(sigma)
+    return MoveForwardResponse(vh=vh, prob=prob, new_center_z=z)
 
 
-handler = Mangum(app)
+@app.post("/seek")
+def seek(data: dict = Depends(parse_body)) -> SeekResponse:
+    req = CommonRequest(**data)
+    z = infer_engine.calc_z(req.position, req.basis, req.center_z)
+    return SeekResponse(synth_params=infer_engine.infer_synth_params(z))
+
+
+@app.post("/turn_around")
+def turn_around(data: dict = Depends(parse_body)) -> TurnAroundResponse:
+    req = TurnAroundRequest(**data)
+    return TurnAroundResponse(basis=infer_engine.turn_around(req.vh, req.prob))
+
+
+@app.exception_handler(RequestValidationError)
+async def handler(request: Request, exc: RequestValidationError):
+    print(exc)
+    return JSONResponse(content={}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
